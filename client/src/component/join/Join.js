6e2b90a8d44fedbf222b749ebcd26842d40850cb
ayushjii <pawangoyal6789@gmail.com>
2022-07-29 04:38:51 +0530

    sign

--- client/src/component/join/Join.js
+++ client/src/component/join/Join.js
@@ -1,14 +1,29 @@
 import React from 'react';
+import './Join.css'
+import logo from '../../images/chat.png';
+import {Link} from 'react-router-dom';
+
+let user;
 
 const Join = () => {
+    
+    const sendUser = () => {
+        user = document.getElementById("joinInput").ariaValueMax;
+    }
+    
     return(
         <div className='JoinPage'>
         <div className='JoinContainer'>
-            <h1>Join Page</h1>
+            <img src={logo} alt="" />
+            <h1>CHATTER BOX</h1>
+            <input placeholder='Enter Your Name' type="text" id="joinInput" />
+            <Link to="/chat">
+            <button onClick={sendUser} className='joinbtn'>Login In</button>
+            </Link>
         </div>
-
         </div>
     )
 }
 
 export default Join;
+export {user};
