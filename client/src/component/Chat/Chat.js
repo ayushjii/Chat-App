6f74b14ca381c91635571fd7adf98ddd785c793a
ayushjii <pawangoyal6789@gmail.com>
2022-07-29 05:49:24 +0530

    k

--- client/src/component/Chat/Chat.js
+++ client/src/component/Chat/Chat.js
@@ -1,14 +1,36 @@
-import React from "react";
-import {user} from "../Join/Join";
-
+import React, { useEffect } from "react";
+import { user } from "../Join/Join";
+import socketIO from 'socket.io-client';
+import './Chat.css'
+import {IoSendSharp} from 'react-icons/io5'
+const ENDPOINT = "http://localhost:4500/";
 
 const Chat = () => {
-    return(
-        <div className="chatPage">
-        <div className="chatContainer">
-            
-        </div> </div>
-    )
-}
+    
+    const socket = socketIO(ENDPOINT , { transports: ['websocket']} );
+
+    useEffect(() => {
+        socket.on('connect', () => {
+            alert("connected");
+        })
+        return () => {
+
+        }
+    }, [socket])
+
+  return (
+    <div className="chatPage">
+      <div className="chatContainer">
+        <div className="header"></div>
+        <div className="chatBox"></div>
+        <div className="inputBox">
+          <input type="text" id="chatInput" />
+          <button className="sendbtn">{<IoSendSharp size={35} />}</button>
+        </div>
+        {user}
+      </div>
+    </div>
+  );
+};
 
 export default Chat;
